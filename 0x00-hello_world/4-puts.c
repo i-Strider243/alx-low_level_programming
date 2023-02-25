803b4fa92a033fab3f2c3e2c836c0a92ebd7b2a7
i-Strider243 <simongabriel243@gmail.com>
2023-02-25 00:18:44 -0800

    Updated 0x00-hello_world files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,13 @@
 /**
  * main - Entry point
  *
+ * Description: 'the program\'s description'
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
