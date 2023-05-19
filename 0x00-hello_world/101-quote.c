3b4d9b6d314b20a6ba65b4e770b8081cfbb1d6b2
Simon Gabriel <simongabriel243@gmail.com>
2023-05-18 18:24:51 -0700

    Add quote file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Start point
+ *
+ * Description: This uses putchar()
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
