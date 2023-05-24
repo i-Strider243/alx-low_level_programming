9dde7c57db913d43c371f7e7b8dee892f342fe7d
Simon Gabriel <simongabriel243@gmail.com>
2023-05-24 05:35:49 -0700

    Update quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Start point
  *
- * Description: This uses putchar()
+ * Description: This uses write()
  *
  * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
