c5095a3b003eb19da95e91b0d079b8f4e3377bf7
Simon Gabriel <simongabriel243@gmail.com>
2023-05-29 14:45:32 -0700

    Add string length file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a given string
+ * @str: string variable
+ * Return: Always 0
+ */
+
+int _strlen(char *str)
+{
+	return (strlen(str));
+}
