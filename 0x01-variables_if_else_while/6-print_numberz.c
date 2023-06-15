2d5ff6afb8bb61598357973141da7cdfd7d69c92
Simon Gabriel <simongabriel243@gmail.com>
2023-06-15 10:10:09 -0700

    Add print numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Print numbers,
+ * without using char
+ * Return 0 (success)
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
