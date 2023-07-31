309533eb568fbb52afe359cae65757398f89ef54
i-strider243 <simongabriel243@gmail.com>
2023-07-31 12:30:58 -0700

    Add static lib files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts(char *str);
+
+/**
+ * main - Entry point
+ * _puts - used to write a line or string to the output
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
